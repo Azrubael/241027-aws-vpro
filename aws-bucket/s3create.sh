99e8c0b5e0f00a2b648e2f5f5925afa3fce51b4c
azrubael <i0638464000@gmail.com>
2024-10-27 20:23:21 +0200

    Minor correction in 'aws-bucket/s3create.sh'.

--- aws-bucket/s3create.sh
+++ aws-bucket/s3create.sh
@@ -9,6 +9,6 @@ aws s3api create-bucket --bucket $BUCKET_NAME --region us-east-1
 # Verify the bucket was created
 if aws s3api head-bucket --bucket $BUCKET_NAME; then
     echo "Bucket $BUCKET_NAME created successfully in us-east-1 region."
-esle
+else
     echo "Something went wrong."
 fi
