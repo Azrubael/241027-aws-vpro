f84c571814fa2be1063cd74f684a0d84a0ed1edd
azrubael <i0638464000@gmail.com>
2024-10-28 19:25:11 +0200

    Added -- 'aws-man/s3-scripts/*.sh' with scripts for basic manipulate AWS B3 buckets.

---
+++ aws-man/s3-scripts/s3create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set your desired bucket name
+BUCKET_NAME="az-$(date +%Y%m%d)"
+
+# Create the S3 bucket in us-east-1 region
+aws s3api create-bucket --bucket $BUCKET_NAME --region us-east-1
+
+# Verify the bucket was created
+if aws s3api head-bucket --bucket $BUCKET_NAME; then
+    echo "Bucket $BUCKET_NAME created successfully in us-east-1 region."
+else
+    echo "Something went wrong."
+fi
