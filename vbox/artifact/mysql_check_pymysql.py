7f9030efd6778baa063903a24d69dd7d1d4a1f6e
azrubael <i0638464000@gmail.com>
2024-11-17 13:08:52 +0200

    Updated -- 'python3_dependencies'. Added   -- 'vbox/artifact/mysql_check_pymysql.py'. To diagnistic MySQL server connection.

---
+++ vbox/artifact/mysql_check_pymysql.py
@@ -0,0 +1,46 @@
+import os
+import pymysql
+import sys
+from dotenv import load_dotenv
+
+"""
+The script to connect and make a simple query to MySQL server. It have to reply with MySQL version.
+Before using you have to make some preparations:
+```bash
+pip3 install pymysql
+python3 <this_script_name>.py
+```
+
+Example:
+    $ python3 mysql_check2.py
+Connected successfully
+MySQL Server version: 5.5.68-MariaDB
+"""
+
+def check_mysql_connection(host, port, user, password):
+    try:
+        # An attempt to connect to MySQL server
+        connection = pymysql.connect(host=host, port=port, user=user, password=password)
+        print("Connected successfully")
+
+        # A simple query
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT VERSION()")
+            result = cursor.fetchone()
+            # My SQL server's version output
+            print(f"MySQL Server version: {result[0]}")
+
+    except pymysql.MySQLError as e:
+        print(f"Connection failed: {e}")
+    finally:
+        if 'connection' in locals():
+            connection.close()  # Закрываем соединение
+
+# Задаем хост, порт, пользователя и пароль MySQL
+host = '192.168.56.15'
+port = 3306
+user = 'admin'
+load_dotenv(dotenv_path='../.env/db_env')
+password = os.environ.get('DATABASE_PASS')
+
+check_mysql_connection(host, port, user, password)
